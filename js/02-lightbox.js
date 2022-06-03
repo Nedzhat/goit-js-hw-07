eec2d873bfec09ce12963c27d91e8829a3bf9a56
Nedzhat <nexuak@gmail.com>
2022-06-03 20:59:25 +0300

    added task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const resultGallery = createGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', resultGallery);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionSelector: 'img',
+  captionsData: 'alt',
+});
