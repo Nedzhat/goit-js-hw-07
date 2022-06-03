12f0bda62e8a89b2c3d714ebcac06a8a2a6d5ff9
Nedzhat <nexuak@gmail.com>
2022-06-03 19:02:07 +0300

    up

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+galleryRef.addEventListener('click', onOpenModal);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, description }) => {
+      return `<img src="${preview}" alt="${description}">`;
+    })
+    .join('');
+}
+
+const resultGallery = createGallery(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', resultGallery);
+
+function onOpenModal(event) {
+  console.dir(event.target);
+}
